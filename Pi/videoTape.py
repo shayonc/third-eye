3d45460d6db5d36aee2369d9464aac1db0384cae
Zemann <zemann.schps@gmail.com>
2017-09-16 14:35:28 +0000

    Bug fix

--- Pi/videoTape.py
+++ Pi/videoTape.py
@@ -1,21 +1,20 @@
 # Import the module
 import subprocess
+import time
 
-keepMaking = true
+keepMaking = True
 videoNumber = 0
 
-echo("Press q to quit process.")
+print("Press q to quit process.")
 #this is the command that we wish to run on the command
 #raspivid -o video.h264 -t 10000
-while(keepMaking){
+while(keepMaking):
 	# Set up the echo command and direct the output to a pipe
 	p1 = subprocess.Popen(['raspivid', '-o', 'video%s.h264' % str(videoNumber), '-t', '2000'], stdout=subprocess.PIPE)
-	videoNumber++
+	videoNumber += 1
 	#output = p1.communicate()[0]
 	#print output
 	#host = raw_input(.)
-	if(host == 'q'){
-		keepMaking = false
-	}
-	sleep(10000)
-}
+        #if(host == 'q'):
+		#keepMaking = False
+	time.sleep(10000)
