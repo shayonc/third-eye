c0d1c75ca08b8bb78b0618b5b6c6278b6041e4f1
Yaw Owusu-Ansah <jyoansah@gmail.com>
2017-09-16 13:52:09 -0400

    Creating file upload api

--- PHP/app/Http/Controllers/ApiController.php
+++ PHP/app/Http/Controllers/ApiController.php
@@ -1,56 +1,42 @@
 <?php
-
-
 namespace App\Http\Controllers;
 
-
-use App\Device;
-use Carbon\Carbon;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\File;
+use Illuminate\Http\Request;
+use GuzzleHttp\Client;
 
 class ApiController {
 
 	public function index() {
 
-		// sms format: 0:device_id, 1:loc_x, 2:loc_y, 3:timestamp, 4:amb_temp, 5:humidity, 6:body_temp
+		$file = Request::file('filefield');
 
-		$data = request()->input();
+		if(isset($file)){
 
-		if ( isset( $data[ 'Body' ] )  ) {
-
-			$in_data = explode( ',', $data['Body'] );
-			if(isset($in_data)) {
-
-				$child = Device::find( $in_data['0'] )->child()->first();
-
-				$child->locations()->create( [
-					'timestamp' => Carbon::now(), //$in_data['0'],
-					'geolat'    => $in_data['1'],
-					'geolng'    => $in_data['2'],
-					// 'geoele'    => $in_data['4'],
-				] );
-
-				$child->health_data()->create( [
-					'timestamp'           => Carbon::now(), //$in_data['0'],
-					'body_temperature'    => $in_data['6'],
-					'ambient_temperature' => $in_data['4'],
-					'humidity'            => $in_data['5'],
-				] );
-
-			} else {
-				return response()->json([
-					'Status' => 'FAIL',
-					'Message' => 'Message format wrong'
-				]);
-			}
-
-		} else {
+			$extension = $file->getClientOriginalExtension();
+			Storage::disk('local')->put($file->getFilename().'.'.$extension,  File::get($file));
 
 			return response()->json([
-				'Status' => 'FAIL',
-				'Message' => 'Request does not include body'
+				'Status' => 'OK',
+				'Message' => Storage::disk('local')->url($file->getFilename().'.'.$extension,  File::get($file)),
+				'Words' => $this->getVideoData()
 			]);
-
+		} else {
+			return response()->json([
+				'Status' => 'No File found',
+				'Message' => Storage::disk('local')->url($file->getFilename().'.'.$extension,  File::get($file))
+			]);
 		}
 
 	}
+
+	public function getVideoData(){
+
+		//get from google
+		$words = array("Person", "Buidling", "Thing");
+		return $words;
+
+	}
+
 }
