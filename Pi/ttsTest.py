502d6711cb6ff99e4426e7be54ad431d289040c5
Shayon Chowdhury <shayontac1@gmail.com>
2017-09-24 20:11:37 -0400

    another text-to-speech test

---
+++ Pi/ttsTest.py
@@ -0,0 +1,18 @@
+import json, time, random, math, urllib, urllib2, subprocess, sys
+
+# returns the appropriate google speech url for a particular phrase
+def getGoogleSpeechURL(phrase):
+    googleTranslateURL = "http://translate.google.com/translate_tts?tl=en&"
+    parameters = {'q': phrase}
+    data = urllib.urlencode(parameters)
+    googleTranslateURL = "%s%s" % (googleTranslateURL,data)
+    return googleTranslateURL
+
+
+# output phrase to audio using mplayer
+def speakSpeechFromText(phrase):
+    googleSpeechURL = getGoogleSpeechURL(phrase)
+    subprocess.call(["mplayer",googleSpeechURL], shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+if __name__=='__main__':
+    speakSpeechFromText("Hello World!")
